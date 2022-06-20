0e1547e6786a761b1497231e82c4fdddb407fb45
Gabriel Del Nero <43073074+Gabixel@users.noreply.github.com>
2022-06-20 19:40:47 +0200

    Update SoundboardApi.ts

--- ts/utility/SoundboardApi.ts
+++ ts/utility/SoundboardApi.ts
@@ -3,4 +3,9 @@ class SoundboardApi {
 		// @ts-ignore
 		window.api.openContextMenu(args);
 	}
+
+	public static get isProduction(): boolean {
+		// @ts-ignore
+		return window.api.isProduction;
+	}
 }
