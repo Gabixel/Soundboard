469031918a708e537231071876522351baea1681
Gabriel Del Nero <43073074+Gabixel@users.noreply.github.com>
2023-01-03 17:39:38 +0100

    Move script tags in `<head>` using 'defer'
    Also adds the `onload` event for future use, and removes specific script declaration in `editButtonWindow` preload file

--- windows/editButtonWindow/preload.js
+++ windows/editButtonWindow/preload.js
@@ -4,22 +4,22 @@ contextBridge.exposeInMainWorld("api", {
 	isProduction: process.env.NODE_ENV === "production",
 });
 
-const appendScripts = (...scripts) => {
-	scripts.forEach((s) => {
-		const script = window.document.createElement("script");
-		script.async = false;
-		script.type = "text/javascript";
-		script.src = "../../js/" + s + ".js";
-		window.document.body.appendChild(script);
-	});
-};
+// const appendScripts = (...scripts) => {
+// 	scripts.forEach((s) => {
+// 		const script = window.document.createElement("script");
+// 		script.async = false;
+// 		script.type = "text/javascript";
+// 		script.src = "../../js/" + s + ".js";
+// 		window.document.body.appendChild(script);
+// 	});
+// };
 
-window.onload = () => {
-	appendScripts(
-		"utility/Logger",
-		"utility/SoundboardApi",
-		"editor/FormSubmitter",
-		"editor/ButtonEditor",
-		"editor/Index"
-	);
-};
+// window.onload = () => {
+// 	appendScripts(
+// 		"utility/Logger",
+// 		"utility/SoundboardApi",
+// 		"editor/FormSubmitter",
+// 		"editor/ButtonEditor",
+// 		"editor/Index"
+// 	);
+// };
