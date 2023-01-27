8738607f62aef31486ebb1f4406d0201b0070b93
Gabriel Del Nero <43073074+Gabixel@users.noreply.github.com>
2023-01-27 23:03:49 +0100

    Use backslashes instead of slashes for URLs
    ...or else the main process doesn't show any window when using the "show in file explorer" context menu option

--- src/ts/utility/StringUtilities.ts
+++ src/ts/utility/StringUtilities.ts
@@ -2,7 +2,7 @@ abstract class StringUtilities {
 	public static encodeFilePath(path: string): string {
 		// Local files (at least on Windows) have backslashes instead of forward slashes. This causes problems since JS treats them as escaping characters.
 		return encodeURIComponent(path.replace(/\\/g, "/"))
-			.replace(/%2F/g, "/") // Replace slashes
-			.replace(/%3A/g, ":"); // Replace colons
+			.replace(/%2F/g, "\\") // Replace encoded slashes with backslashes
+			.replace(/%3A/g, ":"); // Decode colons
 	}
 }
