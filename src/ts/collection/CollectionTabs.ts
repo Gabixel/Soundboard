b2419c62c8a1052961dff9419e1cfea99f358dfe
Gabriel Del Nero <43073074+Gabixel@users.noreply.github.com>
2023-07-28 18:19:50 +0200

    Add basic tab generation and capture scroll events

--- src/ts/collection/CollectionTabs.ts
+++ src/ts/collection/CollectionTabs.ts
@@ -18,8 +18,9 @@ class CollectionTabs {
 		this._$addCollectionButton = $controlsContainer.find(
 			"#add-collection-button"
 		) as JQuery<HTMLButtonElement>;
-		
+
 		this.initAddCollectionButtonEvents();
+		this.initTabContainerEvents();
 	}
 
 	private initAddCollectionButtonEvents(): void {
@@ -38,9 +39,55 @@ class CollectionTabs {
 				}
 
 				this._isAddCollectionButtonHeld = isEnterKey;
+
+				this.createCollectionTab();
 			})
 			.on("blur keyup", (_e) => {
 				this._isAddCollectionButtonHeld = false;
 			});
+	}
+
+	private initTabContainerEvents(): void {
+		this._$tabsContainer.on("wheel", (e) => {
+			if(e.ctrlKey) {
+				return;
+			}
+
+			let tabContainer =  this._$tabsContainer[0]
+			
+			const maxScrollWidth = tabContainer.scrollWidth - tabContainer.clientWidth;
+
+			// @ts-ignore
+			let scrollAmount = e.deltaY;
+
+			console.log(scrollAmount);
+			
+		});
+	}
+
+	private createCollectionTab(
+		// id?: number,
+		// name?: string,
+		// buttonsData?: SoundButtonData[]
+	): void {
+		let tab = this.generateCollectionTab(null);
+
+		this._$tabsContainer.append(tab);
+	}
+
+	private generateCollectionTab(
+		id?: number,
+		name?: string
+	): JQuery<HTMLButtonElement> {
+		if(!id) {
+			id = this._$tabsContainer.children("button.tab-button").length;
+		}
+		
+		let tab = $(`<button id="button-collection-tab-${id}">`)
+			.addClass("tab-button")
+			.attr("tabindex", -1)
+			.text(name ?? `Collection ${id + 1}`) as JQuery<HTMLButtonElement>;
+
+		return tab;
 	}
 }
