58518eb05eaa5c975a3fcbf1ae1bbfa591ce4518
Gabriel Del Nero <43073074+Gabixel@users.noreply.github.com>
2023-07-28 14:56:29 +0200

    Change name for collection tabs in DOM and add basic event setup for "+" button

--- src/ts/collection/SoundButtonDataCollection.ts
+++ src/ts/collection/SoundButtonDataCollection.ts
@@ -3,13 +3,40 @@
  */
 class SoundButtonDataCollection {
 	private _$controlsContainer: JQuery<HTMLDivElement>;
+
+	/**
+	 * Keyboard spam prevention.
+	 */
+	private _isAddCollectionButtonHeld: boolean = false;
 	private _$addCollectionButton: JQuery<HTMLButtonElement>;
 
 	constructor($controlsContainer: JQuery<HTMLDivElement>) {
 		this._$controlsContainer = $controlsContainer;
 		this._$addCollectionButton = $controlsContainer.find(
-			">button"
+			">button#add-collection-button"
 		) as JQuery<HTMLButtonElement>;
-		console.log(this._$addCollectionButton);
+		this.initAddCollectionButtonEvents();
+	}
+
+	private initAddCollectionButtonEvents(): void {
+		this._$addCollectionButton
+			.on("keydown mouseup", (e) => {
+				if (this._isAddCollectionButtonHeld) {
+					return;
+				}
+
+				const isEnterKey = e.key === "Enter";
+				const isLeftMouse = e.button === 0;
+
+				// No extra keys involved
+				if (!isEnterKey && !isLeftMouse) {
+					return;
+				}
+
+				this._isAddCollectionButtonHeld = isEnterKey;
+			})
+			.on("blur keyup", (_e) => {
+				this._isAddCollectionButtonHeld = false;
+			});
 	}
 }
